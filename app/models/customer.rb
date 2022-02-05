e46c02013ad893edf8db401dffda400173a2def7
TJ <tjbachorz@gmail.com>
2022-02-05 11:48:32 -0700

    adds validations for customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+    validates :first_name, presence: true, length: { maximum: 255 }
+    validates :last_name, presence: true, length: { maximum: 255 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+    validates :vehicle_type, presence: true, length: { maximum: 255 }
+    validates :vehicle_name, presence: true, length: { maximum: 255 }
+    validates :vehicle_length, presence: true, length: { maximum: 255 }
 end
