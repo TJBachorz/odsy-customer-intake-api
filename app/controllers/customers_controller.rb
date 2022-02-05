157f34e29937054d2e9c62396ec06b1b36ee8935
TJ <tjbachorz@gmail.com>
2022-02-05 14:49:58 -0700

    adds index method for CustomerController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,10 @@
 class CustomersController < ApplicationController
 
+    def index
+        @customers = Customer.all
+        render json: @customers
+    end
+
     def create
         @customer = Customer.new(customer_params)
 
