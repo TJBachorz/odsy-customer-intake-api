f7aca71df022b8b027309eff83e050ded4001315
TJ <tjbachorz@gmail.com>
2022-02-05 11:49:42 -0700

    adds smoke tests for customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    customer = Customer.new(
+      first_name: "test",
+      last_name: "test",
+      email: "test",
+      vehicle_type: "test",
+      vehicle_name: "test",
+      vehicle_length: "test"
+    )
+    expect(customer).to be_valid
+  end
+  it "is not valid without a first name" do
+    customer = Customer.new(first_name: nil)
+    expect(customer).to_not be_valid
+  end
+  it "is not valid without a last name" do
+    customer = Customer.new(last_name: nil)
+    expect(customer).to_not be_valid
+  end
+  it "is not valid without an email" do
+    customer = Customer.new(email: nil)
+    expect(customer).to_not be_valid
+  end
+  it "is not valid without a vehicle type" do
+    customer = Customer.new(vehicle_type: nil)
+    expect(customer).to_not be_valid
+  end
+  it "is not valid without a vehicle name" do
+    customer = Customer.new(vehicle_name: nil)
+    expect(customer).to_not be_valid
+  end
+  it "is not valid without a vehicle length" do
+    customer = Customer.new(vehicle_length: nil)
+    expect(customer).to_not be_valid
+  end
 end
