287356c4d735b1b432a9f04754b7aac222ad468e
TJ <tjbachorz@gmail.com>
2022-02-05 11:57:10 -0700

    adds customer create method with strong params

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,28 @@
 class CustomersController < ApplicationController
+
+    def create
+        @customer = Customer.new(customer_params)
+        if @customer.valid?
+            @customer.save
+            render json: @customer
+        else
+            render json: @customer.errors.full_messages
+        end
+        
+    end
+
+    private
+
+    def customer_params
+        params
+            .require(:customer)
+            .permit(
+                :first_name,
+                :last_name,
+                :email,
+                :vehicle_type,
+                :vehicle_name,
+                :vehicle_length
+            )
+    end
 end
